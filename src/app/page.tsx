bca872cce34733bbd677318aabf3070865241242
Aakarshit07 <aakarshit070@gmail.com>
2024-09-15 13:53:41 +0530

    Add Footer and install 3d cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusciSchoolTestimonials from "@/components/TestimonialsCards";
@@ -13,7 +14,8 @@ export default function Home() {
       <WhyChooseUs />
       <MusciSchoolTestimonials /> 
       <UpcomingWebinars />  
-      <Instructors />  
+      <Instructors /> 
+      <Footer /> 
     </main>
   );
 }
